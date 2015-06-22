de0979e0a9080a6514e6650450cf20d5fb391386
夜阑静 <coderzhcn@126.com>
2015-06-22 20:44:28 +0800

    Revert "springmvc"
    This reverts commit 5932d754424a8b887c3097dbe6b3b9ab3bdd8103.

--- spring-mvc/src/com/ec/biz/common/service/CommonService.java
+++
@@ -1,9 +0,0 @@
-package com.ec.biz.common.service;
-
-import com.ec.biz.common.req.BaseServiceRequest;
-import com.ec.biz.common.resp.BaseServiceResponse;
-
-public interface CommonService {
-
-	public void execute(BaseServiceRequest request,BaseServiceResponse response);
-}
--- spring-mvc/src/com/nf/yun/web/service/HelloService.java
+++
@@ -1,6 +0,0 @@
-package com.nf.yun.web.service;
-
-public interface HelloService {
-
-	public String hello();
-}
